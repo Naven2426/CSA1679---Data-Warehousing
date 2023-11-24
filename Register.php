b0f31bb89a559f643303718d787c8e17a9d796d7
Naven2426 <112463079+Naven2426@users.noreply.github.com>
2023-11-24 17:45:48 +0530

    Add files via upload

---
+++ Register.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password ="";
+$dbname="student";
+$conn=new mysqli($servername,$username,$password,$dbname);
+if(!$conn)
+{
+die('Could not connect: '.mysqli_connect_error());
+}
+echo 'Connected successfully<br/>';
+$stmt = $conn->prepare("INSERT INTO user(name,password) VALUES(?,?)");
+$stmt->bind_param("ss",$u,$p);
+//set parameters and execute
+$u=$_POST["user"];
+$p=$_POST["pass"];
+$stmt->execute();
+echo "Record inserted successfully";
+$stmt->close();
+$conn->close();
+header("refresh:2; url=home.html");
+?>
